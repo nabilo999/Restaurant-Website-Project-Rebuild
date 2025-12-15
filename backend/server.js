cb7118744afee8a69dab54c9f92ca9c019a87c85
nabil <nabil.said54@myhunter.cuny.edu>
2025-12-14 23:31:54 -0500

    work plz

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Serve React static files
+app.use(express.static(path.join(__dirname, "../build")));
 
 // routes
 const menuRoutes = require("./routes/menu");
@@ -15,6 +19,11 @@ const cartsRoutes = require("./routes/Carts");
 app.use("/api/menu", menuRoutes);
 app.use("/api/orders", ordersRoutes);
 app.use("/api/carts", cartsRoutes);
+
+// Serve React app for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_URL || "";
 
 export async function fetchMenu() {
   const res = await fetch(`${API_BASE}/api/menu`);
